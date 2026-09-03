7e3f6df3d500bcc1f97d1324e3b33044aaa5c23a
Sumit Kr. Singh <sumit306singh@gmail.com>
2026-09-03 11:19:05 +0530

    Time: 0 ms (100%), Space: 26.6 MB (33.29%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool validPalindrome(string s) { 
+        int n = s.length();
+        int left = 0;
+        int right =  n-1;
+        while(left < right){
+            if( s[left] != s[right]){
+                return isPalindrome(s , left+1 , right) || 
+                       isPalindrome(s , left , right-1);
+            }
+            left++;
+            right--;
+        }
+        return true;  
+    }
+
+private:
+        bool isPalindrome(string s , int left , int right){
+        while(left < right){
+            if( s[left] != s[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
